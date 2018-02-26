897f3734abb5090bd97d50d080faec40206afc12
Exide <exide@hotmail.com>
2018-02-25 20:50:56 -0800

    Move collision code back into the blob.

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,7 @@ const {Entity, EntityType} = require('./entity');
 const moment = require('moment');
 const Vector = require('victor');
 const quadtree = require('../../utils/Quadtree');
-const {detectCollisions} = require('./collisions');
+const SAT = require('../../utils/sat');
 
 // list of all sessions
 let sessions = [];
@@ -189,6 +189,54 @@ const loop = () => {
 };
 
 setImmediate(loop);
+
+// collision matrix
+// dynamic DOES collide with dynamic
+// static DOES collide with dynamic
+// static DOESNT collide with static
+
+function detectCollisions(sceneGraph) {
+  let collisions = [];
+
+  sceneGraph.get().forEach(entities => {
+    let dynamics = entities.filter(e => e.dynamic);
+    let statics = entities.filter(e => !e.dynamic);
+    let pairs = buildUniquePairs(dynamics);
+
+    statics.forEach(s => {
+      dynamics.forEach(d => {
+        pairs.push([s, d]);
+      });
+    });
+
+    let collidingPairs = pairs.filter(pair => SAT.isColliding(pair[0], pair[1]));
+    collisions.push(...collidingPairs);
+  });
+
+  return collisions.filter(removeDuplicates);
+}
+
+function buildUniquePairs(entities) {
+  let pairs = [];
+
+  if (entities.length < 2) return pairs;
+
+  for (let i = 0; i < entities.length - 1; ++i) {
+    for (let j = i; j < entities.length - 1; ++j) {
+      pairs.push([entities[i], entities[j+1]]);
+    }
+  }
+
+  return pairs;
+}
+
+function removeDuplicates(pair, index, array) {
+  return index === array.findIndex(p => {
+    let same = p[0].id === pair[0].id && p[1].id === pair[1].id;
+    let inverse = p[0].id === pair[1].id && p[1].id === pair[0].id;
+    return same || inverse;
+  });
+}
 
 // collision outcomes
 // dynamic v. dynamic = both entities reverse and cut velocity by 30%
