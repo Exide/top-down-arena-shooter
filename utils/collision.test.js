da3b701a8b49c613f30dbe1ab7545d11582f03d5
Exide <exide@hotmail.com>
2018-03-21 19:51:26 -0700

    Fixed a test.

--- utils/collision.test.js
+++ utils/collision.test.js
@@ -1,27 +1,23 @@
 const collision = require('./collision');
 const {Entity, EntityType} = require('../server/src/entity');
+const {Vector} = require('./vector');
 const {Point} = require('./point');
 const {Edge} = require('./edge');
 
 //  ·   ·   ·   ·
 //
-//  ·   a   c   ·
-//      | .`
-//  ·   e`  ·   ·
+//  ·   |   ·   ·
+//      | +
+//  ·   |`  ·   ·
 //      |'.
-//  ·   b  `d   ·
+//  ·   |  `·   ·
 //
 //  0   ·   ·   ·
 
 test('bounce as expected', () => {
-  let a = new Point(1, 3);
-  let b = new Point(1, 1);
-  let c = new Point(2, 3);
-  let d = new Point(2, 1);
-  let e = new Point(1, 2);
-  let edge = new Edge(a, b);
-  let entity = new Entity(EntityType.ASTEROID, d, 0, 1, 1);
-  entity.velocity = e.clone().subtract(d);
+  let edge = new Edge(new Point(1, 3), new Point(1, 1));
+  let entity = new Entity(EntityType.ASTEROID, new Point(2, 1), 0, 1, 1);
+  entity.velocity = new Vector(-1, 1);
   collision.applyBounce(entity, edge);
-  expect(entity.velocity).toEqual(c.clone().subtract(e));
+  expect(entity.velocity).toEqual(new Vector(0.7, 0.7));
 });
