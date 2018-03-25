9dce04d7f48c199f90cfadccfaa461cf46ecca55
Ryan Zander <exide@hotmail.com>
2018-03-25 13:19:37 -0700

    Updated a unit test to use the new architecture.

--- utils/collision.test.js
+++ utils/collision.test.js
@@ -1,5 +1,9 @@
 const collision = require('./collision');
-const {Entity, EntityType} = require('../server/src/entity');
+const GameObject = require('./GameObject');
+const Transform = require('./Transform');
+const BoundingBox = require('./BoundingBox');
+const RigidBody = require('./RigidBody');
+const Material = require('./Material');
 const {Vector} = require('./vector');
 const {Point} = require('./point');
 const {Edge} = require('./edge');
@@ -16,8 +20,22 @@ const {Edge} = require('./edge');
 
 test('bounce as expected', () => {
   let edge = new Edge(new Point(1, 3), new Point(1, 1));
-  let entity = new Entity(EntityType.ASTEROID, new Point(2, 1), 0, 1, 1);
-  entity.velocity = new Vector(-1, 1);
+  let entity = new GameObject('Ship');
+  entity.addComponent(Transform.builder()
+    .withPosition(new Point(2, 1))
+    .withRotation(0)
+    .build());
+  entity.addComponent(BoundingBox.builder()
+    .withWidth(1)
+    .withHeight(1)
+    .build());
+  entity.addComponent(RigidBody.builder()
+    .withVelocity(new Vector(-1, 1))
+    .build());
+  entity.addComponent(Material.builder()
+    .withFriction(0.5)
+    .withElasticity(0.5)
+    .build());
   collision.applyBounce(entity, edge);
-  expect(entity.velocity).toEqual(new Vector(0.7, 0.7));
+  expect(entity.getComponent('RigidBody').velocity).toEqual(new Vector(0.5, 0.5));
 });
